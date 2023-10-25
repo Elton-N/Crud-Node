f1c76e63c5d2a88076ef0c81ec8fab036599aba3
Elton do Nascimento <BEN0029162@aluno.firjansenaisesi.com.br>
2023-10-25 09:56:17 -0300

    Atualização

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const express = require('express'); 
-const req = require('express/lib/request');
-const app = express()
-const port = 3000
-app.use(express.json());
+const express = require('express'); // passa uma informação do express para constantes express
+const req = require('express/lib/request');  // Use o objeto `req` para obter informações sobre a solicitação
+const app = express() // a constante app recebe o framework express com seus parametros
+const port = 3000     // porta para ser escutada no localhost com o número 3000 
+app.use(express.json()); // usa os recursos do express
 
-let bd = [
+// criação de um banco local em JSON
+let bd = [ //usando array de objetos
     {
-        id:"1",
-        name: "Elton"
+        id:"1", //ID único do usuário
+        name: "Elton" // nome do usuário
     },
     {
         id:"2",
@@ -16,12 +17,14 @@ let bd = [
 ]
 
 
-//get users
+//get users - lista todos os usuários do endereço users
 app.get('/users', (request, response) => {
+  // exibe no formato JSON
   response.json(bd);
 })
 
-app.get('/users/:id', (request, response) => {
+// lista um único usuário com o id mencionado
+app.get('/users/:id', (request, response) => { // 
 
   // pegar o id da requisição
   const idUser = request.params.id;
@@ -31,9 +34,9 @@ app.get('/users/:id', (request, response) => {
 
   // responder a requisição com as info do users
   response.json(user);
+});
 
-
-})
+// Insere através do POST usuário no banco local
 app.post('/users', (request, response) => {
 
   // pegar o id da requisição
@@ -52,7 +55,7 @@ app.post('/users', (request, response) => {
   response.json(bd);
 })
 
-app.delete("/users/:id", (request, response) => {
+app.delete("/users/:id", (request, response) => { // deleta uma informação do banco de dados, referente ao ID mencionado
 
   // pegar o id da requisição
   const idUser = request.params.id;
@@ -66,8 +69,8 @@ app.delete("/users/:id", (request, response) => {
   response.json(bd);
 
 })
-
-app.patch("/users/:id",  (request, response) => {
+//alterar o usuário mencionado pelo ID
+app.patch("/users/:id",  (request, response) => { 
   
   // pegar o id da requisição
   const idUser = request.params.id;
@@ -91,7 +94,7 @@ response.json(bd);
 
 })
 
-
+// Fica escutando a porta 3000
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
